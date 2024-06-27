a02841572e561ea8a7eeb8f97529a1c54c551dfa
kar1221 <khyeong191@gmail.com>
2024-06-27 19:20:18 +0800

    switch to `deepMap` from `react-children-utilities`
    why reinvent the wheel when others have done it

--- src/components/Editable/Editable.tsx
+++ src/components/Editable/Editable.tsx
@@ -2,59 +2,58 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import { twMerge } from "tailwind-merge";
 import { useHovering, useEditing } from "src/components/CustomHooks";
+import { deepMap } from "react-children-utilities";
 import Hidden from "./Hidden";
 import Visible from "./Visible";
-import Always from "./Always";
 
+/**
+ * `Editable` is a component that allows you to create a component that can be edited.
+ * * The component contains three types of children:
+ * * - `Hidden`: This children will be shown when the component is in editing mode.
+ * * - `Visible`: This children will be shown when the component is not in editing mode.
+ */
 function Editable(props: EditableProps) {
-  const { isHovering, handleMouseEnter, handleMouseLeave } = useHovering(false);
+  const { isHovering, handleOnEnter, handleOnLeave } = useHovering(false);
   const { isEditing, toggleIsEditing } = useEditing(false);
 
-  const { children, className } = props;
+  const { children, className, tabindex } = props;
 
-  const renderedChildren = React.Children.map(children, (child) => {
+  const renderedChildren = deepMap(children, (child) => {
     if (!React.isValidElement(child)) return null;
-
-    if (child.type === Hidden && isEditing === true) return child;
-    if (child.type === Visible && isEditing === false) return child;
-    if (child.type === Always) return child;
-
-    return null;
+    if (child.type === Hidden && isEditing === false) return null;
+    if (child.type === Visible && isEditing === true) return null;
+    return child;
   });
 
   const editableStyle = twMerge(
-    `relative border-b-2 trasnition-all duration-150 ease-in-out ${isHovering ? "border-indigo-500" : " border-transparent"}`,
+    `relative border-b-2 trasnition-all duration-150 ease-in-out ${isHovering ? "border-zinc-500" : "border-transparent"}`,
     className
   );
 
   return (
     <div
       className={editableStyle}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleOnEnter}
+      onMouseLeave={handleOnLeave}
+      onFocus={handleOnEnter}
+      onBlur={handleOnLeave}
+      tabIndex={tabindex}
     >
       {renderedChildren}
-      <button
-        type="button"
-        id="edit-button"
-        className={`m-2 items-center absolute right-0 bottom-0 flex justify-center gap-2 rounded-md bg-indigo-600 p-2 font-Poppins font-medium text-zinc-50 transition-opacity duration-150 ease-in-out ${isHovering ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+
+      <Icon
+        icon={isEditing ? "mdi:pencil-off" : "mdi:pencil"}
+        className={`m-2 h-6 w-6 absolute cursor-pointer text-zinc-600 top-0 right-0 transition-opacity duration-150 ease-in-out ${isHovering ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
         onClick={toggleIsEditing}
-      >
-        <Icon
-          icon={isEditing ? "mdi:pencil" : "mdi:pencil-off"}
-          className="flex aspect-square h-5 w-auto"
-        />
-        {isEditing ? "Save" : "Edit"}
-      </button>
+      />
     </div>
   );
 }
 
 interface EditableProps {
-  children?: React.ReactElement<
-    typeof Hidden | typeof Visible | typeof Always
-  >[];
+  children?: React.ReactNode;
   className?: string;
+  tabindex?: number;
 }
 
 export default Editable;
